ee5e84391efc1d69eda25632aa4b7bf8f3c055d3
root-shubh <root.shubh@gmail.com>
2020-07-29 00:57:19 +0530

    working graphiql queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,15 +1,23 @@
 const graphql = require('graphql');
-const { resolveFieldValueOrError } = require('graphql/execution/execute');
-
 const{GraphQLObjectType, GraphQLString, GraphQLSchema}=graphql;
+const _= require('lodash');
+
 const BookType =new GraphQLObjectType ({
     name:'Book',
     fields:()=>({
         id:{type: GraphQLString},
         name:{type: GraphQLString},
-        genre: {type: GraphQLString}
+        genre: {type: GraphQLString},
     })
 });
+
+//dummyData
+var books=[
+    {name: 'ac', genre:'hype',id:'1'},
+    {name: 'ad', genre:'fantasy',id:'2'},
+    {name: 'ae', genre:'adventure',id:'3'},
+    {name: 'af', genre:'action',id:'4'},
+];
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQuerytype',
@@ -18,12 +26,12 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args:{id:{type: GraphQLString}},
             resolve(parent, args){
-                //
+               return _.find(books, {id: args.id});
             }
         }
     }
-})
+});
 
 module.exports = new GraphQLSchema({
     query: RootQuery
-})
+});
